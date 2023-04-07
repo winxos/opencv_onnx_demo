8493b79cb39648a9cd5d8e14eee72b32e5eb8b1b
wvv <winxos@xhu.edu.cn>
2023-04-07 23:16:29 +0800

    add softmax

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,18 +12,22 @@ int main()
     String model_path = "cifar.onnx";
     Net net = readNetFromONNX(model_path);
     vector<string> labels = { "plane","car","bird","cat","deer","dog","frog","horse","ship","truck" };
-    Mat image = imread("5.jpg");
+    Mat image = imread("3.jpg");
 
     Size input_size = Size(224, 224);
     Scalar mean = Scalar(0.5, 0.5, 0.5);
     Mat blob = blobFromImage(image, 1 / 255.0, input_size, mean, true, false);
     net.setInput(blob, "input");
-    Mat prob = net.forward();
-    cout << prob << endl;
-    cout << prob.reshape(1, 1) << endl;
+    Mat outputMat = net.forward();
+    cout << outputMat << endl;
+    cout << outputMat.reshape(1, 1) << endl;
+    Mat softmaxOutput;
+    cv::exp(outputMat, softmaxOutput);
+    softmaxOutput /= cv::sum(softmaxOutput)[0];
+    cout << softmaxOutput << endl;
     double maxVal;
     Point  maxLoc;
-    cv::minMaxLoc(prob.reshape(1, 1), nullptr, &maxVal, nullptr, &maxLoc);
+    cv::minMaxLoc(softmaxOutput.reshape(1, 1), nullptr, &maxVal, nullptr, &maxLoc);
     cout << maxLoc.x <<","<<maxVal<< endl;
     putText(image, labels[maxLoc.x], Point(10, 30), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
     imshow("Result", image);
